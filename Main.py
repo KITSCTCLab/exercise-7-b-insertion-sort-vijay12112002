ba45e2351a1f254ca5dd2debc8ba628d16279c0e
vijay12112002 <110010946+vijay12112002@users.noreply.github.com>
2022-09-13 11:51:25 +0530

    vj

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 from typing import List
 
-def insertionSort(array) -> List[int]:
+def selectionSort(array, size) -> List[int]:
   for step in range(size):
     min_idx = step
     for i in range(step + 1, size):
@@ -9,7 +9,6 @@ def insertionSort(array) -> List[int]:
     (array[step], array[min_idx]) = (array[min_idx], array[step])
   return array
 
-# data = [9, 5, 1, 4, 3]
 input_data = input()
 data = []
 for item in input_data.split(', '):
@@ -17,4 +16,4 @@ for item in input_data.split(', '):
     data.append(int(item))
   elif item.lstrip("-").isnumeric():
     data.append(int(item))
-print(insertionSort(data))
+print(selectionSort(data, len(data)))
