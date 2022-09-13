cb6f878a464735d163156e2cb6c6280de2f323ca
vijay12112002 <110010946+vijay12112002@users.noreply.github.com>
2022-09-13 11:49:42 +0530

    Initial commit

--- Main.py
+++ Main.py
@@ -1,7 +1,13 @@
 from typing import List
 
 def insertionSort(array) -> List[int]:
-  # Write your code here
+  for step in range(size):
+    min_idx = step
+    for i in range(step + 1, size):
+      if array[i] < array[min_idx]:
+        min_idx = i
+    (array[step], array[min_idx]) = (array[min_idx], array[step])
+  return array
 
 # data = [9, 5, 1, 4, 3]
 input_data = input()
